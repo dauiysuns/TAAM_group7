af511d2ec4f61d2f691986244e3eaa4bea21f6e6
GY-gls <gyac444@gmail.com>
2024-07-15 13:10:12 -0400

    Added main screen xml and java files

---
+++ TAAM/app/src/main/java/com/example/taam/CollectionManagementFragment.java
@@ -0,0 +1,44 @@
+package com.example.taam;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+// NOT COMPLETE!!!
+public class CollectionManagementFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_collection_management, container, false);
+
+        Button buttonAdd = view.findViewById(R.id.buttonAdd);
+        Button buttonRemove = view.findViewById(R.id.buttonRemove);
+        Button buttonBack = view.findViewById(R.id.buttonBack);
+
+        buttonAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //loadFragment(new AddItemFragment());
+            }
+        });
+
+        //buttonRemove.setOnClickListener(v -> loadFragment(new RemoveItemFragment()));
+
+        buttonBack.setOnClickListener(v -> getParentFragmentManager().popBackStack());
+
+        return view;
+    }
+
+    private void loadFragment(Fragment fragment) {
+        FragmentTransaction transaction = getParentFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragment_container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+}
---
+++ TAAM/app/src/main/java/com/example/taam/MainScreenFragment.java
@@ -0,0 +1,55 @@
+package com.example.taam;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+
+public class MainScreenFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.activity_main_screen_fragment, container, false);
+
+        Button buttonAdmin = view.findViewById(R.id.buttonAdmin);
+        Button buttonView = view.findViewById(R.id.buttonView);
+        Button buttonSearch = view.findViewById(R.id.buttonSearch);
+
+        buttonAdmin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               // loadFragment(new AdminFragment());
+            }
+        });
+
+        buttonView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              //  loadFragment(new ViewItemFragment());
+            }
+        });
+
+        buttonSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               // loadFragment(new SearchFragment());
+            }
+        });
+
+        return view;
+    }
+
+    private void loadFragment(Fragment fragment) {
+        FragmentTransaction transaction = getParentFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragment_container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+}
