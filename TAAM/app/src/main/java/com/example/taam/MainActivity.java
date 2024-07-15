e3f09df34009cf6e54733200d4c7041d8ddc09ae
Melc2801 <mel.chen2801@gmail.com>
2024-07-15 14:13:44 -0400

    set up firebase

--- TAAM/app/src/main/java/com/example/taam/MainActivity.java
+++ TAAM/app/src/main/java/com/example/taam/MainActivity.java
@@ -8,12 +8,26 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
+    FirebaseDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
+
+        db = FirebaseDatabase.getInstance("https://taam-cfc94-default-rtdb.firebaseio.com/");
+        DatabaseReference lotRef = db.getReference("Items/LotNumber");
+
+        lotRef.child("test item").setValue("test detail");
+
+        DatabaseReference userRef = db.getReference("User");
+
+        userRef.child("test user").setValue("test password");
+
         setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
