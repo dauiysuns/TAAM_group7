46644af2a6bcacb4d0b85d9693ebfb8dcebc2ddd
Melc2801 <mel.chen2801@gmail.com>
2024-07-16 22:53:16 -0400

    set up firebase

---
+++ TAAM/app/src/main/java/com/example/taam/TaamItem.java
@@ -0,0 +1,10 @@
+package com.example.taam;
+
+public abstract class TaamItem {
+    int lotNumber;
+    String name;
+    String category;
+    String period;
+    String description;
+    //missing field for file
+}
