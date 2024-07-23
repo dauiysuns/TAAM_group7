9991eff21d383f3b6156be0be30148f2f5f635e5
Emily93643 <emmilyemily@gmail.com>
2024-07-23 15:15:03 -0400

    feat: setup MVP architecture and add login XML layout

---
+++ TAAM/app/src/main/java/com/example/taam/ui/login/LoginFragmentModel.java
@@ -0,0 +1,49 @@
+package com.example.taam.ui.login;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.taam.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link LoginFragmentModel#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class LoginFragmentModel {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public LoginFragmentModel() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment LoginFragmentModel.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static LoginFragmentModel newInstance(String param1, String param2) {
+        LoginFragmentModel fragment = new LoginFragmentModel();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        return fragment;
+    }
+}
