a9f325ad5011acc2ad63ee8a68365c98582610f4
Emily93643 <emmilyemily@gmail.com>
2024-07-26 01:30:51 -0400

    feat: Added default fragment to backstack

--- TAAM/app/src/main/java/com/example/taam/MainActivity.java
+++ TAAM/app/src/main/java/com/example/taam/MainActivity.java
@@ -8,6 +8,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.taam.ui.login.LoginFragmentView;
 
 public class MainActivity extends AppCompatActivity
 implements DataView
@@ -29,6 +32,12 @@ implements DataView
         Log.v("main activity", "all items");
         model.displayAllItems();
 
+        if (getSupportFragmentManager().findFragmentById(R.id.fragment_container) == null) {
+            // Add default fragment
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+            transaction.add(R.id.fragment_container, new LoginFragmentView()); // Default fragment
+            transaction.commit();
+        }
     }
 
     @Override
