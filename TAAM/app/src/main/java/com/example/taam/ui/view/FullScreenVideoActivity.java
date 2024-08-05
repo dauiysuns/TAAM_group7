b5d35a9f9c82ddccd2d80d6cca51c94383312026
GY-gls <gyac444@gmail.com>
2024-08-04 22:30:08 -0400

    Set up play button for video

--- TAAM/app/src/main/java/com/example/taam/ui/view/FullScreenVideoActivity.java
+++ TAAM/app/src/main/java/com/example/taam/ui/view/FullScreenVideoActivity.java
@@ -1,7 +1,9 @@
 package com.example.taam.ui.view;
 
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.MediaController;
 import android.widget.VideoView;
@@ -14,6 +16,7 @@ public class FullScreenVideoActivity extends AppCompatActivity {
 
     private VideoView videoView;
     private MediaController mediaController;
+    private ImageView playIcon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,9 +25,11 @@ public class FullScreenVideoActivity extends AppCompatActivity {
 
         videoView = findViewById(R.id.fullScreenVideoView);
         mediaController = new MediaController(this);
+        playIcon = findViewById(R.id.playIcon);
 
         setUpVideo();
         setUpCloseButton();
+        setUpPlayFeatures();
     }
 
     private void setUpVideo(){
@@ -37,11 +42,36 @@ public class FullScreenVideoActivity extends AppCompatActivity {
         // Set up media controller
         mediaController.setAnchorView(videoView);
         videoView.setMediaController(mediaController);
-        videoView.setOnPreparedListener(mp -> videoView.pause());
+
+        videoView.setOnCompletionListener(mp -> showStoppedIcon(true));
     }
 
     private void setUpCloseButton(){
         ImageView closeButton = findViewById(R.id.closeVideo);
         closeButton.setOnClickListener(v -> finish());
     }
+
+    private void showStoppedIcon(boolean show) {
+        if(show){
+            playIcon.setVisibility(VideoView.VISIBLE);
+        }
+        else{
+            playIcon.setVisibility(VideoView.GONE);
+        }
+    }
+
+    private void setUpPlayFeatures() {
+        playIcon.setOnClickListener(v -> {
+            videoView.start();
+            showStoppedIcon(false);
+        });
+
+        videoView.setOnClickListener(v -> {
+            if(playIcon.getVisibility() == View.GONE){
+                videoView.pause();
+                showStoppedIcon(true);
+            }
+        });
+    }
+
 }
