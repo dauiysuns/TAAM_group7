5ecc4c4485089ab20e4b55309d52c675b9533744
GY-gls <gyac444@gmail.com>
2024-08-06 00:31:16 -0400

    Debugged editable spinner classes

--- TAAM/app/src/main/java/com/example/taam/database/EditableCategorySpinner.java
+++ TAAM/app/src/main/java/com/example/taam/database/EditableCategorySpinner.java
@@ -4,19 +4,28 @@ import android.content.Context;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
+import com.example.taam.R;
+
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 
 public class EditableCategorySpinner {
-    public static ArrayList<String> categoryList = new ArrayList<>();
+    public static ArrayList<String> categoryList;
 
     private EditableCategorySpinner(){
-        Collections.addAll(categoryList, "jade", "another category");
     }
 
     public static void setUpSpinnerAdapter(Context context, Spinner spinner){
+        insertDefaultCategories(context);
+        Collections.addAll(categoryList, "Another Category");
         ArrayAdapter<String> spinnerAdapterCat = new ArrayAdapter<>(context, android.R.layout.simple_spinner_item, categoryList);
         spinnerAdapterCat.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(spinnerAdapterCat);
+    }
+
+    private static void insertDefaultCategories(Context context){
+        String[] categoriesArray = context.getResources().getStringArray(R.array.categories_array);
+        categoryList = new ArrayList<>(Arrays.asList(categoriesArray));
     }
 }
--- TAAM/app/src/main/java/com/example/taam/database/EditablePeriodSpinner.java
+++ TAAM/app/src/main/java/com/example/taam/database/EditablePeriodSpinner.java
@@ -4,19 +4,28 @@ import android.content.Context;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
+import com.example.taam.R;
+
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 
 public class EditablePeriodSpinner {
-    public static ArrayList<String> periodList = new ArrayList<>();
+    public static ArrayList<String> periodList;
 
     private EditablePeriodSpinner(){
-        Collections.addAll(periodList, "xia", "another period");
     }
 
     public static void setUpSpinnerAdapter(Context context, Spinner spinner){
+        insertDefaultPeriods(context);
+        Collections.addAll(periodList, "Another Period");
         ArrayAdapter<String> spinnerAdapterCat = new ArrayAdapter<>(context, android.R.layout.simple_spinner_item, periodList);
         spinnerAdapterCat.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(spinnerAdapterCat);
+    }
+
+    private static void insertDefaultPeriods(Context context){
+        String[] categoriesArray = context.getResources().getStringArray(R.array.periods_array);
+        periodList = new ArrayList<>(Arrays.asList(categoriesArray));
     }
 }
