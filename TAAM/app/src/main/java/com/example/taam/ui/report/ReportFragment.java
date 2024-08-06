bbfb671fd1df3b0a294c98ed98f7b39fb866c74a
GY-gls <gyac444@gmail.com>
2024-08-06 14:30:02 -0400

    Implemented modified spinner in ReportFragment

--- TAAM/app/src/main/java/com/example/taam/ui/report/ReportFragment.java
+++ TAAM/app/src/main/java/com/example/taam/ui/report/ReportFragment.java
@@ -146,22 +146,26 @@ public class ReportFragment extends Fragment implements PDFHandler.PDFCallback, 
             textViewForItem.setVisibility(View.VISIBLE);
             byItemText.setVisibility(View.GONE);
             periodSpinner.setVisibility(View.GONE);
+            userInput = categorySpinner.getItemAtPosition(0).toString().toLowerCase();
         } else if (selected.equals("Period") || selected.equals("Period with Description and Picture only")) {
             periodSpinner.setVisibility(View.VISIBLE);
             textViewForItem.setVisibility(View.VISIBLE);
             byItemText.setVisibility(View.GONE);
             categorySpinner.setVisibility(View.GONE);
+            userInput = periodSpinner.getItemAtPosition(0).toString().toLowerCase();
         } else if (selected.equals("All Items")) {
             byItemText.setVisibility(View.GONE);
             textViewForItem.setVisibility(View.GONE);
             categorySpinner.setVisibility(View.GONE);
             periodSpinner.setVisibility(View.GONE);
+            userInput = "";
         } else{
             byItemText.setText("");
             byItemText.setVisibility(View.VISIBLE);
             textViewForItem.setVisibility(View.VISIBLE);
             categorySpinner.setVisibility(View.GONE);
             periodSpinner.setVisibility(View.GONE);
+            userInput = "";
         }
     }
 }
